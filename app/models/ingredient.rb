ddae62e46c2746f5698cef9e32cf5e265c39a95f
Kuba Wróbel <sklerenchyma@gmail.com>
2014-09-04 11:56:04 +0200

    used ingredient can't be deleted

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,11 @@ class Ingredient < ActiveRecord::Base
   has_many :recipes, through: :ingredient_for_recipes
 
   validates :name, presence: true, uniqueness: true
+
+  before_destroy :ingredient_in_use
+
+  protected
+  def ingredient_in_use
+    recipes.empty?
+  end
 end
