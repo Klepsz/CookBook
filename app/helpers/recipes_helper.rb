cb7277c5a5ac3e0d25ce39c0d63add323a4c6ba7
Kuba Wróbel <sklerenchyma@gmail.com>
2014-09-04 12:41:57 +0200

    displaying ingredients in recipe view

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,3 +1,5 @@
 module RecipesHelper
-
+  def show_ingredient(ingredient_for_recipe)
+    "#{ingredient_for_recipe.name} #{ingredient_for_recipe.amount_with_unit}"  
+  end
 end
--- app/models/ingredient_for_recipe.rb
+++ app/models/ingredient_for_recipe.rb
@@ -6,4 +6,13 @@ class IngredientForRecipe < ActiveRecord::Base
   validates :amount, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0, less_than:1000 }
   validates :ingredient_id, uniqueness: { scope: :recipe_id }
+
+  def name
+    ingredient.name
+  end
+
+  def amount_with_unit
+    "#{amount} #{unit.name}"
+  end
+
 end
