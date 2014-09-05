773b7a5f4aee4bda42ad43c715a0d86ccf7547d0
Kuba Wróbel <sklerenchyma@gmail.com>
2014-09-05 15:08:29 +0200

    recipes model test

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -30,7 +30,6 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
-    debugger
       if @recipe.save
         redirect_to @recipe
       else
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -12,6 +12,29 @@ class RecipeTest < ActiveSupport::TestCase
   end
 
   test "tagged_or_all should return tagged properly when tag is present" do
-     assert false, "NAPISZ TESTY ŁOBUZIE"
+    recipes = Recipe.all
+    assert_equal 0, Recipe.count
+    recipe = Recipe.create(name: "Pizza", description: "Very very tasty", directions: "directions",
+                          servings: 4, vegan: false, user_id: 1)
+    recipe.tag_list.add "Classic"
+    recipe.save
+    assert_equal [recipe], recipes.tagged_or_all("Classic")
   end
+
+  test "tagged_or_all return proper tag from among others tags" do
+    recipes = Recipe.all
+    recipe1 = Recipe.create(name: "Pizza1", description: "Very very tasty", directions: "directions",
+                          servings: 4, vegan: false, user_id: 1)
+    recipe1.tag_list.add "Classic"
+    recipe1.save
+    recipe2 = Recipe.create(name: "Pizza2", description: "Very very tasty", directions: "directions",
+                          servings: 4, vegan: false, user_id: 1)
+    recipe2.tag_list.add "Hot"
+    recipe2.save
+    recipe3 = Recipe.create(name: "Pizza3", description: "Very very tasty", directions: "directions",
+                          servings: 4, vegan: false, user_id: 1)
+    assert_equal [recipe1], recipes.tagged_or_all("Classic")
+  end
+
+
 end
